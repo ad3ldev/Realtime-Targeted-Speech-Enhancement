e2ab1430b977d977f13d97ccb6bd29a9d8c04a4a
Muhammad Magdy <muh.magdy1@gmail.com>
2024-01-23 10:56:54 +0200

    Update for tuple

--- rttse/models/base_model.py
+++ rttse/models/base_model.py
@@ -42,9 +42,8 @@ class BaseModel(pl.LightningModule):
             loss_dict[f'{phase}/{loss_name}'] = loss_fn(y_hat, y) * self.loss_weights.get(loss_name, 1)
             # Check if the result is a tuple
             if isinstance(loss_dict[f'{phase}/{loss_name}'], tuple):
-                l_total += sum(loss_dict[f'{phase}/{loss_name}'])
-            else:
-                l_total += loss_dict[f'{phase}/{loss_name}']
+                loss_dict[f'{phase}/{loss_name}'] = sum(loss_dict[f'{phase}/{loss_name}'])
+            l_total += loss_dict[f'{phase}/{loss_name}']
 
         if len(self.losses) > 1: loss_dict[f'{phase}/l_total'] = l_total
         return loss_dict
