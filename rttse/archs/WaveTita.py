031b6fc335d68bc370bf541c4d29cd5f8cd0b95d
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2024-03-19 02:31:48 +0200

    same last commit

--- rttse/archs/WaveTita.py
+++ rttse/archs/WaveTita.py
@@ -11,11 +11,14 @@ class WaveTita(torch.nn.Module):
         super(WaveTita, self).__init__(*args, **kwargs)
         self.device = self.detect_device()
         self.speaker_embedder = self.load_speaker_embedder()
-        self.speech_enhancer = torch.compile(speech_enhancer, mode='max-autotune')
+        self.speech_enhancer = speech_enhancer
+
         if initial_weights:
             self.speech_enhancer.load_state_dict(torch.load(initial_weights))
             get_root_logger().info(f"Loaded weights from {initial_weights}")
-            
+
+        self.speech_enhancer = torch.compile(self.speech_enhancer, mode='max-autotune')
+    
     
     def detect_device(self) -> str:
         return "cuda" if torch.cuda.is_available() else "cpu"
