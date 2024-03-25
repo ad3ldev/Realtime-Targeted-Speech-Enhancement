ca7c38af7475365982b57594dc75c0860fface15
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2024-03-25 11:30:30 +0200

    trying to fix checkpoint bug

--- rttse/train.py
+++ rttse/train.py
@@ -26,8 +26,8 @@ def setup_model(model_cfg, training_cfg):
 def setup_trainer(trainer_cfg, tb_logger):
     seed_everything(trainer_cfg.manual_seed)
     callbacks = hydra.utils.instantiate(trainer_cfg.callbacks)
-    # callbacks = [list(cb.values())[0] for cb in callbacks]
-    trainer = Trainer(**trainer_cfg['trainer_args'], logger=tb_logger, callbacks=list(callbacks.values()))
+    callbacks = [list(cb.values())[0] for cb in callbacks]
+    trainer = Trainer(**trainer_cfg['trainer_args'], logger=tb_logger, callbacks=callbacks)
     return trainer, trainer_cfg.get('checkpoint_path')
 
 
