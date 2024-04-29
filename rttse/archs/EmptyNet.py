d8d022639f7b0424b2cb1007d80a8d3f47a7ba46
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2024-04-29 12:20:39 +0300

    fixed another typo in EmptyNet model

--- rttse/archs/EmptyNet.py
+++ rttse/archs/EmptyNet.py
@@ -1,8 +1,8 @@
 import torch.nn as nn
 
-class EmtpyNet(nn.Module):
+class EmptyNet(nn.Module):
     def __init__(self):
-        super(EmtpyNet, self).__init__()
+        super(EmptyNet, self).__init__()
 
     def forward(self, x, **kwargs):
         return x
