da3866a9e515608c2fae050dd16d92e9176fe580
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2024-06-20 12:04:40 +0300

    moved read audio signal in ECAPA-TDNN to cuda

--- rttse/embedders/ECAPATDNN.py
+++ rttse/embedders/ECAPATDNN.py
@@ -1,13 +1,15 @@
 from embedders.EmbedderWrapper import EmbedderWrapper
 from speechbrain.inference.speaker import EncoderClassifier
-from torch import Tensor
+from torch import Tensor, cuda
 import torchaudio
 
 class ECAPATDNN(EmbedderWrapper):
     def __init__(self):
         super(ECAPATDNN, self).__init__()
+        self.device = "cuda" if cuda.is_available() else "cpu"
         self.model = EncoderClassifier.from_hparams(source="speechbrain/spkrec-ecapa-voxceleb")
 
     def embed(self, file_path: str) -> Tensor: 
         signal, _ = torchaudio.load(file_path)
+        signal = signal.to(self.device)
         return self.model.encode_batch(signal).squeeze(0)
