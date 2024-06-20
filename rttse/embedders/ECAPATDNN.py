1382b754a6c47f744be220e279b8f246b8a2345a
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2024-06-20 14:43:04 +0300

    modified batch embedding in ECAPA

--- rttse/embedders/ECAPATDNN.py
+++ rttse/embedders/ECAPATDNN.py
@@ -1,6 +1,6 @@
 from embedders.EmbedderWrapper import EmbedderWrapper
 from speechbrain.inference.speaker import EncoderClassifier
-from torch import Tensor, cuda
+from torch import Tensor, cuda, cat
 import torchaudio
 
 from utils.logger import get_root_logger
@@ -17,3 +17,17 @@ class ECAPATDNN(EmbedderWrapper):
         get_root_logger().info(f'ECAPA-TDNN signal tensor device: {signal.device}')
         get_root_logger().info(f'ECAPA-TDNN model device: {self.model.device}')
         return self.model.encode_batch(signal).squeeze(0)
+
+    def embed_batch(self, file_paths: list) -> Tensor:
+        audios_tensors = []
+        min_length = float('inf')
+
+        for file_path in file_paths:
+            signal = torchaudio.load(file_path)[0].to(self.device)
+            min_length = min(min_length, signal.size(1))
+            audios_tensors.append(signal)
+
+        for i in range(len(audios_tensors)):
+            audios_tensors[i] = audios_tensors[i][:, :min_length]
+
+        return self.model.encode_batch(cat(audios_tensors)).squeeze(1)
