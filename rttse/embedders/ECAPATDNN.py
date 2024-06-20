e008d3fe03079ae5332dad80addef9c31fd30b5f
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2024-06-20 14:57:15 +0300

    modified single embedding

--- rttse/embedders/ECAPATDNN.py
+++ rttse/embedders/ECAPATDNN.py
@@ -14,20 +14,21 @@ class ECAPATDNN(EmbedderWrapper):
     def embed(self, file_path: str) -> Tensor: 
         signal, _ = torchaudio.load(file_path)
         signal = signal.to(self.device)
+        get_root_logger().info(f'ECAPA-TDNN signal tensor shape: {signal.shape}')
         get_root_logger().info(f'ECAPA-TDNN signal tensor device: {signal.device}')
         get_root_logger().info(f'ECAPA-TDNN model device: {self.model.device}')
-        return self.model.encode_batch(signal).squeeze(0)
+        return self.model.encode_batch(signal).squeeze(1)
 
-    def embed_batch(self, file_paths: list) -> Tensor:
-        audios_tensors = []
-        min_length = float('inf')
+    # def embed_batch(self, file_paths: list) -> Tensor:
+    #     audios_tensors = []
+    #     min_length = float('inf')
 
-        for file_path in file_paths:
-            signal = torchaudio.load(file_path)[0].to(self.device)
-            min_length = min(min_length, signal.size(1))
-            audios_tensors.append(signal)
+    #     for file_path in file_paths:
+    #         signal = torchaudio.load(file_path)[0].to(self.device)
+    #         min_length = min(min_length, signal.size(1))
+    #         audios_tensors.append(signal)
 
-        for i in range(len(audios_tensors)):
-            audios_tensors[i] = audios_tensors[i][:, :min_length]
+    #     for i in range(len(audios_tensors)):
+    #         audios_tensors[i] = audios_tensors[i][:, :min_length]
 
-        return self.model.encode_batch(cat(audios_tensors)).squeeze(1)
+    #     return self.model.encode_batch(cat(audios_tensors)).squeeze(1)
