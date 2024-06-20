3d8c919b1a5e0b4847bfe4c827888c35352cce8d
Youhanna-Yousry <youhanna.yousry.2580@gmail.com>
2024-06-20 15:09:47 +0300

    some modifications

--- rttse/embedders/ECAPATDNN.py
+++ rttse/embedders/ECAPATDNN.py
@@ -19,16 +19,16 @@ class ECAPATDNN(EmbedderWrapper):
         get_root_logger().info(f'ECAPA-TDNN model device: {self.model.device}')
         return self.model.encode_batch(signal).squeeze(1)
 
-    # def embed_batch(self, file_paths: list) -> Tensor:
-    #     audios_tensors = []
-    #     min_length = float('inf')
+    def embed_batch(self, file_paths: list) -> Tensor:
+        audios_tensors = []
+        min_length = float('inf')
 
-    #     for file_path in file_paths:
-    #         signal = torchaudio.load(file_path)[0].to(self.device)
-    #         min_length = min(min_length, signal.size(1))
-    #         audios_tensors.append(signal)
+        for file_path in file_paths:
+            signal = torchaudio.load(file_path)[0].to(self.device)
+            min_length = min(min_length, signal.size(1))
+            audios_tensors.append(signal)
 
-    #     for i in range(len(audios_tensors)):
-    #         audios_tensors[i] = audios_tensors[i][:, :min_length]
+        for i in range(len(audios_tensors)):
+            audios_tensors[i] = audios_tensors[i][:, :min_length]
 
-    #     return self.model.encode_batch(cat(audios_tensors)).squeeze(1)
+        return self.model.encode_batch(cat(audios_tensors)).squeeze(1)
