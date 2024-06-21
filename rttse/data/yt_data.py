ff9e2fa631b9bff017cea8d3216fefc126849952
Karim Alaa <karimalaa1912@gmail.com>
2024-06-21 14:05:06 +0300

    added crop flag

--- rttse/data/yt_data.py
+++ rttse/data/yt_data.py
@@ -21,13 +21,13 @@ class YTData(torch.utils.data.Dataset):
         return os.path.join(self.data_root, path)
 
 
-    def load_audio(self, path, normalize=True):
+    def load_audio(self, path, normalize=True, crop=False):
         audio, sr = torchaudio.load(self.get_data_path(path), normalize=normalize)
         if sr != self.sr:
             audio = T.Resample(sr, self.sr, dtype=audio.dtype)(audio.float())
             # audio = F.resample(audio, sr, self.sr, dtype=audio.dtype)
         
-        if self.crop_length_sec:
+        if crop and self.crop_length_sec:
             crop_length = int(self.crop_length_sec * self.sr)
             assert crop_length < len(audio)
 
@@ -43,8 +43,8 @@ class YTData(torch.utils.data.Dataset):
         ## mix AClean, BClean with one of 3 ratios (0.667, 0.444, 0.296)
         ## return ARef, mixed, AClean
         aRef   = self.load_audio(data_record['speakerAReference'])
-        aClean = self.load_audio(data_record['speakerAClean'])
-        bClean = self.load_audio(data_record['speakerBClean'])
+        aClean = self.load_audio(data_record['speakerAClean'], crop=True)
+        bClean = self.load_audio(data_record['speakerBClean'], crop=True)
 
 
         mix_level = random.choices(self.mix_levels, k=1)[0]
