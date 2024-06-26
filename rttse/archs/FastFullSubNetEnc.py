cb6aaf8e0fe04d1a5d8fa1487efc7e36579fcb4d
Karim Alaa <karimalaa1912@gmail.com>
2024-06-26 21:16:12 +0300

    ReLU

--- rttse/archs/FastFullSubNetEnc.py
+++ rttse/archs/FastFullSubNetEnc.py
@@ -99,7 +99,7 @@ class FastFullSubNetEmbedding(FullSubNetBaseModel):
             nn.ReLU(),
             nn.Linear(num_mels * 4, num_mels * 2),
             nn.LayerNorm(num_mels * 2),
-            nn.Relu()
+            nn.ReLU(),
         )
 
         if weight_init:
